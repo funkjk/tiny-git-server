4e5ec7514652c16c829e510d4bfc154f2ebd0d36
funkjk <kenjift@gmail.com>
2025-05-05 11:19:42 +0900

    update output dir

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,19 @@
 import typescript from '@rollup/plugin-typescript';
 import del from 'rollup-plugin-del'
+const { LERNA_PACKAGE_NAME } = process.env
+const packageName = LERNA_PACKAGE_NAME.replace("@tiny-git-server/", "")
+const outputDir = '../../dist/'+packageName
 export default [
   {
     input: ['./src/index.ts'],
     output: {
       format: "cjs",
-      dir: 'dist',
+      dir: outputDir,
       sourcemap: true,
     },
     plugins: [
       del(),
-      typescript({ compilerOptions: { outDir: "./dist" } })
+      typescript({ compilerOptions: { outDir: outputDir } })
     ]
   }
 ]
